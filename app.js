358167ace286708e536de033ce596ccb929fd579
luis <yongchun1683@gmail.com>
2018-08-05 13:12:17 +0800

    setup local MongoDB

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-const express = require('express'),
-      app     = express();
+const express     = require('express'),
+      app         = express(),
+      mongoose    = require('mongoose');
+
+//! ---------- DB setup ---------- //
+mongoose.connect('mongodb://localhost:27017/yt_base', { useNewUrlParser: true});
+let docSchema = new mongoose.Schema({
+    name: String,
+    link: String,
+    type: String,
+    desc: String
+});
+let Doc = mongoose.model('Doc', docSchema);
+
+// * hardcode data for preview
+// Doc.create({
+//         name: 'Traversy Media',
+//         link: 'https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA',
+//         type: 'Programming',
+//         desc: 'Traversy Media features the best online web development and programming tutorials for all of the latest web technologies including Node.js, Angular 2, React.js, PHP, Rails, HTML, CSS and much more'
+// }, (err, doc)=>{
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(doc);
+//     }
+// })
 
 app.set('view engine', 'ejs');
 
-let bases = [
-    {
-        name: 'Traversy Media',
-        link: 'https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA',
-        type: 'Programming',
-        desc: 'Traversy Media features the best online web development and programming tutorials for all of the latest web technologies including Node.js, Angular 2, React.js, PHP, Rails, HTML, CSS and much more'
-    },
-    {
-        name: 'Traversy Media',
-        link: 'https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA',
-        type: 'Programming',
-        desc: 'Traversy Media features the best online web development and programming tutorials for all of the latest web technologies including Node.js, Angular 2, React.js, PHP, Rails, HTML, CSS and much more'
-    },
-    {
-        name: 'Traversy Media',
-        link: 'https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA',
-        type: 'Programming',
-        desc: 'Traversy Media features the best online web development and programming tutorials for all of the latest web technologies including Node.js, Angular 2, React.js, PHP, Rails, HTML, CSS and much more'
-    },
-    {
-        name: 'Traversy Media',
-        link: 'https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA',
-        type: 'Programming',
-        desc: 'Traversy Media features the best online web development and programming tutorials for all of the latest web technologies including Node.js, Angular 2, React.js, PHP, Rails, HTML, CSS and much more'
-    },
-]
 
 //! ---------- Routes ---------- //
 
@@ -38,9 +37,17 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/index', (req, res)=>{
-    res.render('docs', {
-        bases: bases
-    });
+    //* retrieve data form DB
+    Doc.find({}, (err, docs)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.render('docs', {
+                docs: docs
+            });
+        }
+    })
+
 })
 
 
